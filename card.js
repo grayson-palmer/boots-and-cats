6c47b5697d8868cc9880c99ae11ed45548dd7d72
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-03 11:43:02 -0700

    Outline card class

---
+++ card.js
@@ -0,0 +1,12 @@
+class Card {
+  constructor(name, image) {
+    this.id = ++counter;
+    this.name = name;
+    this.image = image;
+    this.matched = false;
+  }
+  match() {
+    
+  }
+}
+var counter = 0;
