793d0b5ff44c4b67720b4392ed12927acd910276
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-04 11:54:26 -0700

    Add placeholders for methods

--- deck.js
+++ deck.js
@@ -1,18 +1,17 @@
 class Deck {
-  constructor() {
-    this.cards = this.populateDeck();
+  constructor(cards) {
+    this.cards = cards;
     this.matchedCards = [];
     this.selectedCards = [];
     this.matches = [];
   }
-  populateDeck() {
-    var cards = [];
-    var name = ['boot-1', 'boot-2', 'boot-3', 'cat-1', 'cat-2'];
-    var image = ['./assets/boot-1.jpg', './assets/boot-2.jpg', './assets/boot-3.jpg', './assets/cat-1.jpg', './assets/cat-2.jpg'];
-    for (var i = 0; i < 5; i++) {
-      cards.push(new Card(name[i], image[i]));
-      cards.push(new Card(name[i], image[i]));
-    }
-    return cards;
+  shuffle() {
+
+  }
+  checkSelectedCards() {
+    
+  }
+  moveToMatched() {
+
   }
 }
