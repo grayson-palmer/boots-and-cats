513e3e5b8e2888c5ecdb3192048537f92908c93c
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-04 10:44:08 -0700

    Add function to populate deck with cards

--- deck.js
+++ deck.js
@@ -1,17 +1,17 @@
 class Deck {
   constructor() {
-    this.cards = populateDeck();
+    this.cards = this.populateDeck();
     this.matchedCards = [];
     this.selectedCards = [];
     this.matches = [];
   }
   populateDeck() {
-    var cards = []
-    var names = ['boot-1', 'boot-2', 'boot-3', 'cat-1', 'cat-2'];
-    var faces = ['./assets/boot-1.jpg', './assets/boot-2.jpg', './assets/boot-3.jpg', './assets/cat-1.jpg', './assets/cat-2.jpg'];
+    var cards = [];
+    var name = ['boot-1', 'boot-2', 'boot-3', 'cat-1', 'cat-2'];
+    var image = ['./assets/boot-1.jpg', './assets/boot-2.jpg', './assets/boot-3.jpg', './assets/cat-1.jpg', './assets/cat-2.jpg'];
     for (var i = 0; i < 5; i++) {
-      cards.push(new Card(names[i], faces[i]))
-      cards.push(new Card(names[i], faces[i]))
+      cards.push(new Card(name[i], image[i]));
+      cards.push(new Card(name[i], image[i]));
     }
     return cards;
   }
