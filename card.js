987026582850034704fbc7f4f52e85c30acab2f2
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-04 10:44:26 -0700

    Removed card counter

--- card.js
+++ card.js
@@ -1,11 +1,8 @@
 class Card {
   constructor(name, image) {
-    this.id = ++counter;
     this.name = name;
     this.image = image;
     this.matched = false;
-  }
-  match() {
-
+    this.hasFlippled = false;
   }
 }
