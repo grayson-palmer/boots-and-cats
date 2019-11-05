fc4078884109ed4ab87ab8775b730dd192ddb12e
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-05 16:50:45 -0700

    Add logic for getting round info and save to storage outline

--- player.js
+++ player.js
@@ -2,9 +2,19 @@ class Player {
   constructor(name) {
     this.name = name;
     this.roundCount = 0;
-    this.rounds = [];
+    this.time = null;
   }
   addRoundCount() {
-    this.RoundCount++;
+    this.roundCount++;
+  }
+  pushToRounds(time) {
+    this.addRoundCount();
+    this.time = time;
+  }
+  saveToStorage() {
+    var previousBoard = JSON.parse(localStorage.getItem('leaderBoard')) || [];
+    var storePlayer = JSON.stringify({'name': this.name, 'time': timeDisplay()});
+    previousBoard.push(storePlayer);
+    localStorage.setItem('leaderBoard', previousBoard);
   }
 }
