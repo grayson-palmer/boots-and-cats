bc31a6e3ac3de200e6a3d77485dec92abafb0112
Grayson Palmer <gpalmer1385@gmail.com>
2019-11-05 09:49:28 -0700

    Refactor logic card match, Add match count, Add remove card fns

--- deck.js
+++ deck.js
@@ -3,7 +3,7 @@ class Deck {
     this.cards = cards;
     this.matchedCards = [];
     this.selectedCards = [];
-    this.matches = [];
+    // this.matches = [];
   }
   
   shuffle() {
@@ -12,14 +12,16 @@ class Deck {
   
   checkSelectedCards() {
     if (this.selectedCards[0].image === this.selectedCards[1].image) {
-      this.matchedCards.push(this.selectedCards[0], this.selectedCards[1]);
+      this.moveToMatched();
       this.selectedCards = [];
+      removeCards();
     } else {
       unflipCards();
     }
   }
-
+  
   moveToMatched() {
-
+    this.matchedCards.push(this.selectedCards[0], this.selectedCards[1]);
+    updateMatchCount();
   }
 }
